4bbbea8d37033f285760685a6c1d14824e4ab800
telegrambot81 <telegrambot81@gmail.com>
2016-04-08 16:14:32 +0430

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,14 +245,14 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {120629055,106789762},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+channel : @kingbot_team
+sodu : @Oo_mehdi_shar_khoffash_revale_oO
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -317,11 +317,11 @@ Only sudo users can run this command
 !bc 123456789 Hello !
 This command will send text to [group_id]
 
-ch: @Nod32team
+ch: @kingbot_team
 
 ]],
     help_text = [[
-NOD32 Commands list :
+bot Commands list :
  
 1-banhammer list ^
  
@@ -518,7 +518,7 @@ NOD32 Commands list :
 you can use both "/" and "!" 
 .شما میتوانید از ! و / استفاده کنید
 
-channel: @Nod32Team
+channel: @kingbot_team
 
 G00D LUCK ^_^
 ]]
